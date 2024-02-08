d8fa9aaadbdd2253cac90afa22d7b9b7a7194eec
Laura1608 <131393433+Laura1608@users.noreply.github.com>
2024-02-08 14:30:50 +0100

    Fine-tuning data cleaning

--- api_project_flightprices.py
+++ api_project_flightprices.py
@@ -15,12 +15,9 @@ url = "https://sky-scrapper.p.rapidapi.com/api/v1/flights/getPriceCalendar"
 
 querystring = {"originSkyId": "VLC", "destinationSkyId": "AMS", "fromDate": "2021-01-01"}
 
-headers = {
-    "X-RapidAPI-Key": config.api_key,
-    "X-RapidAPI-Host": config.api_host
-}
+headers = {"X-RapidAPI-Key": config.api_key, "X-RapidAPI-Host": config.api_host}
 
-# Retrieve data and change format
+# Retrieve data from API and parsing text
 response = requests.get(url, headers=headers, params=querystring)
 data = response.json()
 results = data.get('data').get('flights').get('days')
@@ -35,26 +32,30 @@ for dictionary in results:
     price = dictionary['price']
     parsed_data.append([date, category, price])
 
-# Create dataframe with flight data in columns
+# Create a dataframe with columns
 column_names = ['date', 'group', 'price']
 df = pd.DataFrame(parsed_data, columns=column_names)
 
-# Convert datatype of date column to datetime
+# Convert date column to datetime
 df['date'] = pd.to_datetime(df['date'])
+
+# Split datetime column into year, month, and day
 df['year'] = df['date'].dt.year.astype('Int64')
 df['month'] = df['date'].dt.month.astype('Int64')
 df['day'] = df['date'].dt.day.astype('Int64')
+
+# Additionally, add weekdays
 df['weekday'] = df['date'].dt.weekday
 
-# Replace integer values of weekdays by strings
+# Replace integer values of weekdays by text
 df['weekday'] = df['weekday'].astype(str)
 df['weekday_text'] = df['weekday'].str.replace('0', 'monday').str.replace('1', 'tuesday').str.replace('2', 'wednesday').str.replace('3', 'thursday').str.replace('4', 'friday').str.replace('5', 'saturday').str.replace('6', 'sunday')
 
-# Replace string values of groups by integers
+# Replace values of group column by integers
 df['group_num'] = df['group'].str.replace('high', '2').str.replace('medium', '1').str.replace('low', '0')
 df['group_num'] = df['group_num'].astype('Int64')
 
-
+# Calculating correlations
 # Create new dataframe with variables we'd like to know correlation of
 df_c = df[['group_num', 'price', 'year', 'month', 'day', 'weekday']]
 
@@ -70,4 +71,4 @@ px.imshow(data_corr).show()
 # Correlation >=0.3 and <0.5 means there is a weak relationship between the variables.
 # Correlation <0.3 means there is no relationship between the variables.
 
-# Findings: ..........
+# Findings: As expected, there is a strong correlation between the variables 'price' and 'group', as they indicated the price range. Further no correlations found.
